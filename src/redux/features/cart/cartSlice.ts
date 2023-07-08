f3f702260a58c075f2c7253e8af46e17ecf55edc
mjrgeorge <mjrgeorge@gmail.com>
2023-07-08 14:01:00 +0600

    existing site redux setup done

---
+++ src/redux/features/cart/cartSlice.ts
@@ -0,0 +1,32 @@
+import { IProduct } from '@/types/globalTypes';
+import { createSlice } from '@reduxjs/toolkit';
+import type { PayloadAction } from '@reduxjs/toolkit';
+
+interface ICart {
+  products: IProduct[];
+}
+
+const initialState: ICart = {
+  products: [],
+};
+
+const cartSlices = createSlice({
+  name: 'cart',
+  initialState,
+  reducers: {
+    addToCart: (state, action: PayloadAction<IProduct>) => {
+      const existing = state.products.find(
+        (product) => product._id === action.payload._id
+      );
+      if (existing) {
+        console.log(`Already added`);
+        existing.quantity = existing.quantity! + 1;
+      } else {
+        state.products.push({ ...action.payload, quantity: 1 });
+      }
+    },
+  },
+});
+
+export const { addToCart } = cartSlices.actions;
+export default cartSlices.reducer;
