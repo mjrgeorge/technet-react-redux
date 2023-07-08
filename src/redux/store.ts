edab9acc6a684b36a8bde9656c13a4ea89d880ba
mjrgeorge <mjrgeorge@gmail.com>
2023-07-08 19:59:07 +0600

    rtk query done

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cart/cartSlice';
 import cartProduct from './features/product/productSlice';
+import { api } from './api/apiSlice';
 
 const store = configureStore({
   reducer: {
     cart: cartReducer,
     product: cartProduct,
+    [api.reducerPath]: api.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
